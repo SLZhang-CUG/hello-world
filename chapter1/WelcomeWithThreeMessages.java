33a55d8a40acf3d26eccc483d8c7db7b81c7711d
SLZhang-CUG <zhangshuoliang007@gmail.com>
2018-01-02 16:16:49 +0800

    WelcomeWithThreeMessages

---
+++ chapter1/WelcomeWithThreeMessages.java
@@ -0,0 +1,7 @@
+public class WelcomeWithThreeMessages{
+	public static void main(String[] args){
+		System.out.println("Programing is fun!");
+		System.out.println("Fundamentals firsts");
+		System.out.println("Problem Driven");
+	}
+}

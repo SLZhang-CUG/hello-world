6eaa6798f96a11d04490384bca6bda87d99dc0f9
SLZhang-CUG <zhangshuoliang007@gmail.com>
2018-01-02 16:42:34 +0800

    ShowLogicErrors

--- chapter1/ShowLogicErrors.java
+++ chapter1/ShowLogicErrors.java
@@ -1,5 +1,5 @@
 public class ShowLogicErrors{
-	public static void main(Sting[] args){
+	public static void main(String[] args){
 		System.out.println("Celsius 35 is Fahrenheit degree ");
 		System.out.println((9 / 5) * 35 +32);
 	}

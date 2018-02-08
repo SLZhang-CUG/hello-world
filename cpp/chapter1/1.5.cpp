f79cc4ba1cfe7d224fa426376c158b1ec1e989a7
SLZhang-CUG <zhangshuoliang007@gmail.com>
2018-02-08 14:03:53 +0800

    练习 1.5

---
+++ cpp/chapter1/1.5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	int v1 = 3, v2 = 7;
+	std::cout << "The num of v1 is " << v1 << std::endl;
+	std::cout << "The num of v2 is " << v2 << std::endl;
+	std::cout << "The sum of v1 and v2 is " << v1 + v2 << std::endl;
+}
